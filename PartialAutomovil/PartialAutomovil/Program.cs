1822a03f262e16d6afb2ea4316bcc6358d915da0
Candida Carolina Sandino <142435272+CandidaSandino05@users.noreply.github.com>
2024-05-12 13:37:47 -0600

    Comando de dos archivos

---
+++ PartialAutomovil/PartialAutomovil/Program.cs
@@ -0,0 +1,36 @@
+﻿using PartialAutomovil;
+using System.Drawing;
+using System.Security;
+
+partial class Programs
+{
+    static void Main(string[] args) 
+    {
+        //Integramos una instancia para la informacion de los vehiculos y poder utilizar los metodos 
+        //Mostramos el vehiculo ingresado y sus referencias 
+
+        Vehiculo Auto = new Vehiculo( "Toyota", "HSD3490", "HILUX", "Gris", 80, 30,true );
+        Console.WriteLine("Marca: {0}", Auto.marca);
+        Console.WriteLine("Placa: {0}", Auto.placa);
+        Console.WriteLine("Capacidad Del Tanque de Combustible: {0}", Auto.capacidadtanqueCombustible);
+
+        //Implementamos el metodo creado en la primer clase parcial 
+        //Indicar el numero de kilometros a conducir (50 y 300)
+
+        Auto.Conducir(50);
+        Auto.Conducir(300);
+
+        //Mandamos a llamar el metodo para poder llenar nuestro tanque de combustible 
+
+        Auto.RestablecerCantCombustible(80);
+
+
+        //Mostrarle al usuario el nivel de combustible restante luego de declarar la distancia recorrida
+
+        Console.WriteLine("El nivel de Combustible Restante es de: {0} Litros.", Auto.nivelcombustible);
+
+    }
+
+
+
+}
---
+++ PartialAutomovil/PartialAutomovil/Vehiculo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialAutomovil
+{
+    partial class Vehiculo
+    {
+        public string marca { get; set; }
+        public string placa { get; set; }
+        public string modelo { get; set; }
+        public string color { get; set; }
+        public int capacidadtanqueCombustible { get; set; }
+        public int rendCombustible { get; set; }
+        public int nivelcombustible { get; set; }
+
+        public Vehiculo(string Marca, string Placa, string Modelo, string Color, int CapacidadDeTanqueCombustible, int RendimientoDeCombustible, bool v) 
+        {
+            this.marca = Marca;
+            this.placa = Placa; 
+            this.modelo = Modelo;
+            this.color = Color;
+            this.capacidadtanqueCombustible = CapacidadDeTanqueCombustible;
+            this.rendCombustible = RendimientoDeCombustible;
+            this.nivelcombustible = 0;
+            
+        }
+
+        public bool CombustibleSuficiente(int Distancia) 
+        {
+            return nivelcombustible >= Distancia / rendCombustible;
+          
+        }
+
+        public void Conducir(int Distancia)
+        {
+            try
+            {
+                if (CombustibleSuficiente(Distancia))
+                {
+                    nivelcombustible -= Distancia / rendCombustible;
+                    Console.WriteLine("{0} Kilometros Conducidos, Su nivel de combustible restante es: {1} Litros.", Distancia, nivelcombustible);
+
+                }
+                else
+                {
+                    Console.WriteLine("No posee combustible suficiente para conducir {0} Kilometros.", Distancia);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error: Su rendimiento de combustible es cero (0).");
+
+
+            }
+
+
+
+        }
+
+
+        public void RestablecerCantCombustible(int Cantidad) 
+        {
+            if (nivelcombustible + Cantidad <= capacidadtanqueCombustible)
+            {
+                nivelcombustible += Cantidad;
+                Console.WriteLine("Se añadio {0} Litros de Combustible," +
+                                 " El nuevo nivel de Combustible es: {1} Litros.", Cantidad, nivelcombustible);
+
+            }
+            else 
+            {
+                Console.WriteLine("No puede añadir esa cantidad de combustible , excede los limites de capacidad del tanque.");
+
+            }
+        
+        }
+    }
+}
